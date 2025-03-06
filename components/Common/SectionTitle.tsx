f1b2134adc39ca6fcdba6becc5deffb926a02b68
chengivan <chengoleivan@gmail.com>
2025-03-06 12:47:37 +0300

    Referencing the mesh image that's on Cloudinary and styled the section title paragraph

--- components/Common/SectionTitle.tsx
+++ components/Common/SectionTitle.tsx
@@ -18,7 +18,7 @@ const SectionTitle = ({
     <div>
         <Image
         alt="SafariDesk website mesh accent image"
-        src="/images/mesh-1img.png"
+        src="https://res.cloudinary.com/dpxpjd5fb/image/upload/v1741254292/mesh-1img_xye0ib.png"
         fill
         className="absolute opacity-50 -z-10"
         />
@@ -32,7 +32,7 @@ const SectionTitle = ({
         <h2 className="animate-fade-up animate-duration-[1600ms] mb-4 text-3xl font-bold !leading-tight text-black dark:text-white sm:text-4xl md:text-[45px]">
           {title}
         </h2>
-        <p className="text-base animate-fade-down animate-duration-[1600ms] !leading-relaxed text-body-color md:text-lg">
+        <p className="text-base animate-fade-down animate-duration-[1600ms] !leading-relaxed text-body-color dark:text-white md:text-lg">
           {paragraph}
         </p>
       </div>
