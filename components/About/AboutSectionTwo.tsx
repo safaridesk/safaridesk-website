b061b171a4f2de9a1dc046af0a0b63ca0f5b39c6
chengivan <chengoleivan@gmail.com>
2025-03-06 18:42:43 +0300

    Modified the second about section's image and added the about css file

--- components/About/AboutSectionTwo.tsx
+++ components/About/AboutSectionTwo.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import "./about.css";
 
 const AboutSectionTwo = () => {
   return (
@@ -16,7 +17,7 @@ const AboutSectionTwo = () => {
                 height={500}
                 width={500}
                 fill={false}
-                className="animate-flip-up drop-shadow-three animate-duration-[1600ms] dark:hidden dark:drop-shadow-none"
+                className="lightImageTwo animate-flip-up drop-shadow-three animate-duration-[1600ms] dark:hidden dark:drop-shadow-none"
               />
               <Image
                 src="https://res.cloudinary.com/dpxpjd5fb/image/upload/v1741198644/safaridesk-infographic-two_s6dtq3.png"
@@ -24,7 +25,7 @@ const AboutSectionTwo = () => {
                 height={500}
                 width={500}
                 fill={false}
-                className="animate-flip-up drop-shadow-three animate-duration-[1600ms] hidden dark:block dark:drop-shadow-none"
+                className="darkimageTwo animate-flip-up drop-shadow-three animate-duration-[1600ms] hidden dark:block dark:drop-shadow-none"
               />
             </div>
           </div>
